858f27ef825ef52df3fceb6c9151b93b68bacc01
kcinnoy <yonnick00@gmail.com>
2021-06-27 15:22:28 +0100

    added logic

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyAppState extends State<MyApp> {
   double _numberFrom = 0;
   String _startMeasure = 'meters';
   String _convertedMeasure = 'kilometers';
+  String _resultMessage = '';
 
   final List<String> _measures = [
     'meters',
@@ -32,6 +33,17 @@ class MyAppState extends State<MyApp> {
     'miles': 5,
     'pounds (lbs)': 6,
     'ounces': 7,
+  };
+
+  final dynamic _formulas = {
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
   };
 
   @override
@@ -147,14 +159,19 @@ class MyAppState extends State<MyApp> {
                 // Text((_numberFrom == null) ? '' : _numberFrom.toString())
                 SizedBox(height: 40.0),
                 ElevatedButton(
-                  child: Text(
-                    'Convert',
-                    style: inputStyle,
-                  ),
-                  onPressed: () => true,
+                  child: Text('Convert', style: inputStyle),
+                  onPressed: () {
+                    if (_startMeasure.isEmpty ||
+                        _convertedMeasure.isEmpty ||
+                        _numberFrom == 0) {
+                      return;
+                    } else {
+                      convert(_numberFrom, _startMeasure, _convertedMeasure);
+                    }
+                  },
                 ),
                 SizedBox(height: 40.0),
-                Text((_numberFrom == null) ? '' : _numberFrom.toString(),
+                Text((_resultMessage == null) ? '' : _resultMessage,
                     style: labelStyle),
                 SizedBox(height: 40.0),
               ],
@@ -163,5 +180,22 @@ class MyAppState extends State<MyApp> {
         ),
       ),
     );
+  }
+
+  void convert(double value, String from, String to) {
+    int? nFrom = _measuresMap[from];
+    int? nTo = _measuresMap[to];
+    var multiplier = _formulas[nFrom.toString()][nTo];
+    var result = value * multiplier;
+
+    if (result == 0) {
+      _resultMessage = 'This conversion cannot be performed';
+    } else {
+      _resultMessage =
+          '${_numberFrom.toString()} $_startMeasure are ${result.toString()} $_convertedMeasure';
+    }
+    setState(() {
+      _resultMessage = _resultMessage;
+    });
   }
 }
